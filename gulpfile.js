442c4c5a6359dbe8fec26accf12a1e1f80d7d461
duginsea <dugang1002@sina.com>
2016-07-21 13:52:24 +0800

    0721

--- gulpfile.js
+++ gulpfile.js
@@ -1,74 +1,102 @@
-//引入gulp
-var gulp = require('gulp');
-//引入各个模块
-var sass = require('sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    livereload = require('gulp-livereload');
+//  //引入gulp
+//  var gulp = require('gulp');
+// //引入各个模块
+// var sass = require('gulp-sass'),
+// 	autoprefixer = require('gulp-autoprefixer'),
+//     minifycss = require('gulp-minify-css'),
+//     jshint = require('gulp-jshint'),
+//     uglify = require('gulp-uglify'),
+//     imagemin = require('gulp-imagemin'),
+//     rename = require('gulp-rename'),
+//     concat = require('gulp-concat'),
+//     notify = require('gulp-notify'),
+//      cache = require('gulp-cache'),
+//     livereload = require('gulp-livereload');
 
-// Styles任务
-gulp.task('styles', function() {
-    //编译sass
-    return gulp.src('stylesheets/main.scss')
-    .pipe(sass())
-    //添加前缀
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    //保存未压缩文件到我们指定的目录下面
-    .pipe(gulp.dest('stylesheets'))
-    //给文件添加.min后缀
-    .pipe(rename({ suffix: '.min' }))
-    //压缩样式文件
-    .pipe(minifycss())
-    //输出压缩文件到指定目录
-    .pipe(gulp.dest('assets'))
-    //提醒任务完成
-    .pipe(notify({ message: 'Styles task complete' }));
+// // Styles任务
+// gulp.task('styles', function() {
+//     //编译sass
+//     return gulp.src('stylesheets/main.scss')
+//     .pipe(sass())
+//     //添加前缀
+//     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+//     //保存未压缩文件到我们指定的目录下面
+//     .pipe(gulp.dest('stylesheets'))
+//     //给文件添加.min后缀
+//     .pipe(rename({ suffix: '.min' }))
+//     //压缩样式文件
+//     .pipe(minifycss())
+//     //输出压缩文件到指定目录
+//     .pipe(gulp.dest('assets'))
+//     //提醒任务完成
+//     .pipe(notify({ message: 'Styles task complete' }));
+// });
+
+// // Scripts task
+// gulp.task('scripts', function() {
+//     //js代码校验
+//     return gulp.src('javascripts/*.js')
+//     .pipe(jshint())
+//     .pipe(jshint.reporter('default'))
+//     //js代码合并
+//     .pipe(concat('all.js'))
+//     //给文件添加.min后缀
+//     .pipe(rename({ suffix: '.min' }))
+//     //压缩脚本文件
+//     .pipe(uglify())
+//     //输出压缩文件到指定目录
+//     .pipe(gulp.dest('assets'))
+//     //提醒任务完成
+//     .pipe(notify({ message: 'Scripts task complete' }));
+// });
+
+//  //Images task
+//  gulp.task('images', function() {
+//   return gulp.src('images/*')
+//     .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+//     .pipe(gulp.dest('images'))
+//     .pipe(notify({ message: 'Images task complete' }));
+// });
+
+// // Watch
+// gulp.task('watch', function() {
+//   // Watch .scss files
+//   gulp.watch('stylesheets/*.scss', ['styles']);
+//   // Watch .js files
+//   gulp.watch('javascripts/*.js', ['scripts']);
+//   // Watch image files
+//   gulp.watch('images/*', ['images']);
+//   // Create LiveReload server
+//   livereload.listen();
+//   // Watch any files in assets/, reload on change
+//   gulp.watch(['assets/*']).on('change', livereload.changed);
+// });
+
+// var gulp        = require('gulp');
+// var browserSync = require('browser-sync').create();
+
+// // Static server
+// gulp.task('browser-sync', function() {
+//     browserSync.init({
+//         server: {
+//             baseDir: "./"
+//         }
+//     });
+// });
+
+var gulp = require('gulp'),
+	imagemin = require('gulp-imagemin'),
+	pngquant = require('imagemin-pngquant');
+
+gulp.task('minImage',function(){
+	gulp.src('images/**/*.png')
+	.pipe(imagemin(
+		  {
+		  	optimizationLevel: 5,
+		  	progressive: true,
+		  	use:[pngquant({quality:'30-50'})]
+		  }
+		))
+	.pipe(gulp.dest('imagesDist'));
+
 });
-
-// Scripts task
-gulp.task('scripts', function() {
-    //js代码校验
-    return gulp.src('javascripts/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    //js代码合并
-    .pipe(concat('all.js'))
-    //给文件添加.min后缀
-    .pipe(rename({ suffix: '.min' }))
-    //压缩脚本文件
-    .pipe(uglify())
-    //输出压缩文件到指定目录
-    .pipe(gulp.dest('assets'))
-    //提醒任务完成
-    .pipe(notify({ message: 'Scripts task complete' }));
-});
-
-// Images task
-gulp.task('images', function() {
-  return gulp.src('images/*')
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('images'))
-    .pipe(notify({ message: 'Images task complete' }));
-});
-
-// Watch
-gulp.task('watch', function() {
-  // Watch .scss files
-  gulp.watch('stylesheets/*.scss', ['styles']);
-  // Watch .js files
-  gulp.watch('javascripts/*.js', ['scripts']);
-  // Watch image files
-  gulp.watch('images/*', ['images']);
-  // Create LiveReload server
-  livereload.listen();
-  // Watch any files in assets/, reload on change
-  gulp.watch(['assets/*']).on('change', livereload.changed);
-});
-
--- javascripts/index.js
+++ javascripts/index.js
@@ -1,11 +1,68 @@
-$('div.letter').mouseover(function(event) {
+$('div.letter').mouseenter(function(event) {
 	/* Act on the event */
 
 	$(this).stop().children('span').each(function(index){
-		event.stopPropagation();
+		
 		$(this).animate({"opacity": 0.1},200)
-			   .delay(100*index)
 			   .animate({"opacity":1},200);
-		``
+		
+		
+}).stopPropagation();
 });
+
+var sliderbarFlag = 0;
+//单击侧边的三道杠标签，出现侧边栏
+$(".leftbar").click(function(event) {
+	/* Act on the event */
+	var $slidebar = $(".slidebar");
+	if(sliderbarFlag === 0) {
+	sliderbarFlag = 1;
+	$slidebar.css("display","block")
+			 .css("opacity","1")
+			 .animate({"left": "-200px"},1)
+			 .animate({"left": "0"},200);
+	 $('.xicon').css('display','block')
+	 		   .animate({"left":"-60px"},1)
+	 		   .animate({"left":"140px"},200);
+	}
+
+
 });
+
+//单击交叉时，关闭侧边栏
+$(".xicon").click(function(event){
+	if(sliderbarFlag === 1) {
+	sliderbarFlag = 0;
+	var $slidebar = $(".slidebar");
+	$slidebar.animate({"left": "0"},1)
+			 .animate({"left": "-250px"},300);
+	var $xicon = $(".xicon");
+		$xicon.animate({"left":"-80px"},300);
+			  // .css("display","none");
+			   
+	}
+
+})
+
+//响应式布局时用来显示窗口大小的临时窗口
+$(window).resize(function(){
+	var width = $(window).width();
+	var child = $("<span>" + width + "</span>");
+	$('#test').empty().append(child);
+})
+
+
+//点击图片展示栏的小按钮时,切换不同图片
+var $rightcir = $('.rightcir');
+var $showpic = $('.showpic');
+$rightcir.each(function(index){
+	var realindex = index+1;
+	$(this).click(function(){
+		$showpic.css("background-image","url(../images/gallery-pic/"+realindex+".jpg)");
+	});
+});
+
+
+
+
+
